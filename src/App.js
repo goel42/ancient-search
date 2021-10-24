085d50adcfd6fea34506dbd534992e7712afb598
Anujraaj Goyal <anujraajg@gmail.com>
2021-10-25 00:19:45 +0530

    init barebones search ready. json-ified sample data and create elastic search indices

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { ReactiveBase, 
+DataSearch, 
+ReactiveList, 
+ResultCard } from '@appbaseio/reactivesearch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <section className="container">
+      <ReactiveBase url="http://localhost:9200" app="ancient" enableAppbase={false}>
+
+
+          <DataSearch
+              componentId="searchbox"
+              dataField={[
+                {
+                  field: "character.keyword",
+                  weight: 3,
+                },
+              ]}
+              placeholder="Search for IAST characters..."
+            />
+          <ReactiveList
+            componentId="results"
+            size={8}
+            pagination={true}
+            react={{
+              and: [
+                "searchbox",
+              ],
+            }}
+            render={({ data }) => (
+              <ReactiveList.ResultCardsWrapper>
+                {data.map((item) => (
+                  <ResultCard key={item._id}>
+                  <ResultCard.Image src={item.url}/>
+                    <ResultCard.Title
+                      dangerouslySetInnerHTML={{
+                        __html: item.file_name,
+                      }}
+                    />
+                    <ResultCard.Description>
+                      <div>
+                        {item.character}
+                      </div>
+                    </ResultCard.Description>
+                  </ResultCard>
+                ))}
+              </ReactiveList.ResultCardsWrapper>
+            )}
+          />
+      </ReactiveBase>
+      </section>
+    );
+  }
 }
-
 export default App;
